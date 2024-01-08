934dd8d3d2a6cce9e27442ea7c7073c93d85beb5
priyankkmt <149180584+priyankkmt@users.noreply.github.com>
2024-01-08 19:02:30 +0530

    Update main.cpp

--- SatelliteOrbitSimulator/src/main.cpp
+++ SatelliteOrbitSimulator/src/main.cpp
@@ -2,10 +2,11 @@
 #include <QtWidgets/QApplication>
 #include "Simulator.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) // start of the program
 {
-    QApplication a(argc, argv);
-    Simulator w;
-    w.show();
-    return a.exec();
+    // Initaite Application object and show simulator window
+    QApplication applicationObj(argc, argv);
+    Simulator simulatorObj;
+    simulatorObj.show();
+    return applicationObj.exec();
 }
