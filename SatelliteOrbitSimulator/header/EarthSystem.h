0d094155a638a158340c48c3bc91363dddb35cda
priyankkmt <149180584+priyankkmt@users.noreply.github.com>
2024-01-08 19:19:28 +0530

    Update EarthSystem.h

--- SatelliteOrbitSimulator/header/EarthSystem.h
+++ SatelliteOrbitSimulator/header/EarthSystem.h
@@ -22,20 +22,27 @@ private:
 	void drawOrbitingSatellite(std::vector<float>& vertices, std::vector<float>& colors);
 
 private:
-	float mSatelliteRadius   = { 0.2f }; // set it to 0.2 for visualization
-	float mSatelliteDistance = { 6.371f }; // Earth's radius in Km
-	float mSatelliteAngle    = { 0.0f };
+	float mSatelliteRadius = 0.2f ; // set it to 0.2 for visualization
+	float mSatelliteDistance = 6.371f ; // Earth's radius in Km
+	float mSatelliteAngle = 0.0f ;
 
 	float mOrbitVelocity;
 	float mSatelliteSpeed;
 
-	float mEscapeVelocity = { 11.2f }; // 11.2 Km/s for earth
-	float mAltitude       = { 6.371f };
+	float mEscapeVelocity = 11.2f; // 11.2 Km/s for earth
+	float mAltitude = 6.371f;
 
 	Point3D mSatelliteColor;
 
 	std::vector<float> mCurrentVertices;
 	std::vector<float> mCurrentColors;
+
+	const float PI = 3.1415926f;
+	const float FALL_VELOCITY = 0.1f;
+	const float ESCAPE_VELOCITY = 0.09f;
+	const float EARTH_RADIUS = 6.371f;
+	const float GRAVITATIONAL_CONSTANT = 398583.96f;
+	const float EARTH_RADIUS_KM = 6371.0f;
 
 	Sphere* sphere;
 };
