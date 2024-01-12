695e58a25c0e3086b2671f97b3e12f93b147e4da
priyankkmt <149180584+priyankkmt@users.noreply.github.com>
2024-01-12 15:52:03 +0530

    Update Simulator.cpp

--- SatelliteOrbitSimulator/src/Simulator.cpp
+++ SatelliteOrbitSimulator/src/Simulator.cpp
@@ -11,14 +11,11 @@
 Simulator::Simulator(QWidget* parent) : QMainWindow(parent)
 {
     // Set up the user interface
-	setupUi();
+    setupUi();
     // Connect button clicks to corresponding functions
-	connect(mStartButton, &QPushButton::clicked, this, &Simulator::startBtn);
-	connect(mStopButton, &QPushButton::clicked, this, &Simulator::stopBtn);
-	connect(mResetButton, &QPushButton::clicked, this, &Simulator::resetBtn);
-    connect(mSpeedButton, &QPushButton::clicked, this, &Simulator::addVelocity);
-    connect(mAltitudeButton, &QPushButton::clicked, this, &Simulator::addAltitude);
-    connect(mSizeButton, &QPushButton::clicked, this, &Simulator::addSize);
+    connect(mSimulateButton, &QPushButton::clicked, this, &Simulator::simulateBtn);
+    connect(mStopButton, &QPushButton::clicked, this, &Simulator::stopBtn);
+    connect(mResetButton, &QPushButton::clicked, this, &Simulator::resetBtn);
 }
 
 Simulator::~Simulator()
@@ -27,97 +24,140 @@ Simulator::~Simulator()
 
 void Simulator::setupUi()
 {
-    resize(1200, 900);
-
-    // Create the central widget
+    // Create the central widget with a horizontal layout
     mWidget = new QWidget(this);
-    setCentralWidget(mWidget);
 
-    // Create buttons for Start, Stop, and Reset
-    mStartButton = new QPushButton("Start", mWidget);
-    mStopButton = new QPushButton("Stop", mWidget);
-    mResetButton = new QPushButton("Reset", mWidget);
+    QHBoxLayout* centralLayout = new QHBoxLayout(mWidget);
+
+    QSizePolicy sizePolicy1(QSizePolicy::Minimum, QSizePolicy::Maximum);
+
+    QMargins margins(50, 10, 50, 50);
+    QMargins marginsCentralLayout(10, 10, 10, 10);
 
     // Create an OpenGL rendering window
     mRenderer = new OpenGLWindow(QColor(0, 0, 0), mWidget);
+    centralLayout->addWidget(mRenderer, 1); 
 
-    mRenderer->setGeometry(10, 50, 1000, 800);
+    // Set margins around the OpenGLWidget
+    centralLayout->setContentsMargins(marginsCentralLayout);
 
-    int rightSideX = mRenderer->geometry().right() + 10;
+    // Create a vertical layout for the buttons on the right side
+    QVBoxLayout* rightSideLayout = new QVBoxLayout(mWidget);
+    rightSideLayout->setContentsMargins(margins);
 
-    mStartButton->setGeometry(rightSideX, 210, 75, 24);
-    mStopButton->setGeometry(rightSideX, 250, 75, 24);
-    mResetButton->setGeometry(rightSideX, 290, 75, 24);
+    mSpeedLabel = new QLabel("Km/s");
+    mAltitudeLabel = new QLabel("x100 Km");
+    mSizeLabel = new QLabel("Meter");
 
-    mSpeedButton = new QPushButton("Speed", mWidget);
-    mSpeedButton->setObjectName("mSpeedButton");
-    mSpeedButton->setGeometry(rightSideX + 80, 90, 75, 24);
-
-    mAltitudeButton = new QPushButton("Altitude", mWidget);
-    mAltitudeButton->setObjectName("mAltitudeButton");
-    mAltitudeButton->setGeometry(rightSideX + 80, 130, 75, 24);
-
-    mSizeButton = new QPushButton("Size", mWidget);
-    mSizeButton->setObjectName("mSizeButton");
-    mSizeButton->setGeometry(rightSideX + 80, 170, 75, 24);
+    applyCustomStyleLabel(mSpeedLabel);
+    applyCustomStyleLabel(mAltitudeLabel);
+    applyCustomStyleLabel(mSizeLabel);
 
     mSpeedInput = new QDoubleSpinBox(mWidget);
-    mSpeedInput->setObjectName("mSpeedInput");
-    mSpeedInput->setGeometry(rightSideX , 90, 75, 24);
-
     mAltitudeInput = new QDoubleSpinBox(mWidget);
-    mAltitudeInput->setObjectName("mAltitudeInput");
-    mAltitudeInput->setGeometry(rightSideX , 130, 75, 24);
-
     mSizeInput = new QDoubleSpinBox(mWidget);
-    mSizeInput->setObjectName("mSizeInput");
-    mSizeInput->setGeometry(rightSideX , 170, 75, 24);
 
-    mLabel = new QLabel(mWidget);
-    mLabel->setObjectName("mLabel");
-    mLabel->setGeometry( 60, 10, 221, 31);
+    applyCustomStyleDoubleSpinBox(mSpeedInput);
+    applyCustomStyleDoubleSpinBox(mAltitudeInput);
+    applyCustomStyleDoubleSpinBox(mSizeInput);
 
-    // Set font and text for the title mLabel
-    QFont font;
-    font.setPointSize(15);
-    mLabel->setFont(font);
-    mLabel->setText("Satellite Orbit Simulator");
+    rightSideLayout->addWidget(mSpeedInput);
+    rightSideLayout->addWidget(mSpeedLabel);
+    rightSideLayout->addWidget(mAltitudeInput);
+    rightSideLayout->addWidget(mAltitudeLabel);
+    rightSideLayout->addWidget(mSizeInput);
+    rightSideLayout->addWidget(mSizeLabel);
 
+    mSimulateButton = new QPushButton("Start", mWidget);
+    mStopButton = new QPushButton("Stop", mWidget);
+    mResetButton = new QPushButton("Reset", mWidget);
+
+    mSimulateButton->setSizePolicy(sizePolicy1);
+    mStopButton->setSizePolicy(sizePolicy1);
+    mResetButton->setSizePolicy(sizePolicy1);
+
+    applyCustomStyleButton(mSimulateButton);
+    applyCustomStyleButton(mStopButton);
+    applyCustomStyleButton(mResetButton);
+
+    rightSideLayout->addWidget(mSimulateButton);
+    rightSideLayout->addWidget(mStopButton);
+    rightSideLayout->addWidget(mResetButton);
+
+    rightSideLayout->setAlignment(Qt::AlignTop);
+
+    centralLayout->addLayout(rightSideLayout);
+
+    setCentralWidget(mWidget);
+    //showMaximized();
     setWindowTitle(QCoreApplication::translate("Simulator", "Simulator", nullptr));
 }
 
+// custom style function for label
+void Simulator::applyCustomStyleLabel(QLabel* label) {
+    QString styleSheet = R"(
+        QLabel {
+            color: black;
+            font-size: 16px;
+            font-weight: bold;
+            padding: 2px;
+        }
+    )";
+    label->setStyleSheet(styleSheet);
+}
+
+// custom style function for double spin box
+void Simulator::applyCustomStyleDoubleSpinBox(QDoubleSpinBox* doubleSpinBox) {
+    QString styleSheet = R"(
+        QDoubleSpinBox {
+            background-color: lightgray;
+            border: 2px solid darkgray;
+            border-radius: 5px;
+            padding: 2px;
+        }
+    )";
+
+    doubleSpinBox->setStyleSheet(styleSheet);
+}
+
+// custom style function for button
+void Simulator::applyCustomStyleButton(QPushButton* button) {
+    QString styleSheet = R"(
+        QPushButton {
+            font-size: 14pt;
+            background-color: #1E90FF;   
+            color: white;                
+            border: 2px solid #4169E1;   
+            border-radius: 8px;           
+            padding: 8px 16px;        
+        }
+        QPushButton:hover {
+            background-color: #3F88C5;   
+        }
+        QPushButton:pressed {
+            background-color: #2A6496;  
+        }
+    )";
+    button->setStyleSheet(styleSheet);
+}
+
 // Start button click event handler
-void Simulator::startBtn()
+void Simulator::simulateBtn()
 {
-	mRenderer->startRendering(mVelocity, mSize, mAltitude);
+    mVelocity = mSpeedInput->value();
+    mAltitude = mAltitudeInput->value();
+    mSize = mSizeInput->value();
+    mRenderer->startRendering(mVelocity, mSize, mAltitude);
 }
 
 // Stop button
 void Simulator::stopBtn()
 {
-	mRenderer->stopRevolving();
+    mRenderer->stopRevolving();
 }
 
 // Reset button
 void Simulator::resetBtn()
 {
-	mRenderer->resetPositions();
-}
-
-// Handler for adding velocity from the input field
-void Simulator::addVelocity()
-{
-    mVelocity = mSpeedInput->value();
-}
-
-// Handler for adding altitude from the input field
-void Simulator::addAltitude()
-{
-    mAltitude = mAltitudeInput->value();
-}
-
-// Handler for adding size from the input field
-void Simulator::addSize()
-{
-    mSize = mSizeInput->value();
+    mRenderer->resetPositions();
 }
